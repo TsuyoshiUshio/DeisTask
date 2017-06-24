14ee70fcd3e7f9590d150c962cc581425dd70c23
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-06-25 02:09:33 +0900

    Refactor the task
    Refactor the task. However, I also try to make it async. However, it
    didn't work. This might be a lack of knowledge of typescript async
    programming. I'll try it agen in near future.

--- general.ts
+++ general.ts
@@ -6,66 +6,102 @@ import fs = require('fs');
 import path = require('path');
 import { ToolRunner } from 'vsts-task-lib/toolrunner';
 
-var isWin = tl.osType().match(/^Win/);
-if (isWin) {
-    tl.setResult(tl.TaskResult.Failed, "This task does not work for Windows agent")
-    throw "This task does not work for Windows agent."
+
+
+export class DeisCommand {
+
+  endpoint : string;
+  username : string;
+  password : string;
+  serverURI : string;
+  subCommand: string;
+  multilineArgs: string;
+  deisBinary: string;
+
+    constructor() {
+        this.endpoint = tl.getInput('deisService');
+        this.username = tl.getEndpointAuthorizationParameter(this.endpoint, 'username', true);
+        this.password = tl.getEndpointAuthorizationParameter(this.endpoint, 'password', true);
+        let serverURIWithProtocol :string = tl.getEndpointUrl(this.endpoint, true);
+        this.serverURI = serverURIWithProtocol.replace(/.*:\/\//, '')
+        this.subCommand = tl.getInput('subCommand');
+        this.multilineArgs = tl.getInput('arguments');
+        let deisBinaryDir = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'];
+        this.deisBinary = deisBinaryDir + '/deis'
+    }
+
+checkOSType() {
+    let isWin = tl.osType().match(/^Win/);
+    if (isWin) {
+        tl.setResult(tl.TaskResult.Failed, "This task does not work for Windows agent")
+        throw "This task does not work for Windows agent."
+    }
 }
 
-let endpoint = tl.getInput('deisService');
-
-let username :string = tl.getEndpointAuthorizationParameter(endpoint, 'username', true);
-let password :string = tl.getEndpointAuthorizationParameter(endpoint, 'password', true);
-let serverURIWithProtocol :string = tl.getEndpointUrl(endpoint, true);
-let serverURI = serverURIWithProtocol.replace(/.*:\/\//, '')
-
-let subCommand: string = tl.getInput('subCommand');
-let multilineArgs: string = tl.getInput('arguments');
-
-let bash = new ToolRunner(tl.which('bash', true));
-let downloader = "curl -sSL http://deis.io/deis-cli/install-v2.sh | bash";
-let filename = './.deisdownloader.sh';
-fs.writeFileSync(filename, downloader);
-
-bash.arg(filename);
-try {
-    bash.execSync();
-} catch (err) {
-    tl.setResult(tl.TaskResult.Failed, err);
-    throw "Failed to exec the .deisdownloader.sh which is deis downloader."
-}
-let deisBinaryDir = process.env['SYSTEM_DEFAULTWORKINGDIRECTORY'];
-
-let deisBinary = deisBinaryDir + '/deis'
-
-let deis = new ToolRunner(tl.which(deisBinary, true));
-deis.arg("auth:login");
-deis.arg(serverURI);
-deis.arg("--username=" + username);
-deis.arg("--password=" + password);
-
-try {
-    deis.execSync();
-} catch (err) {
-    tl.setResult(tl.TaskResult.Failed, err);
-    throw "Failed to login using deis auth:login command."
+init() {
+    this.checkOSType();
+    this.downloadDeis();
+    this.login();
 }
 
+downloadDeis()  {
 
-deis = new ToolRunner(tl.which(deisBinary, true));
-deis.arg(subCommand);
-if (multilineArgs) {
-    multilineArgs.split(/\s+/).map(function (x) { deis.arg(x) });
+    let bash = new ToolRunner(tl.which('bash', true));
+    let downloader = "curl -sSL http://deis.io/deis-cli/install-v2.sh | bash";
+    let filename = './.deisdownloader.sh';
+    fs.writeFileSync(filename, downloader);
+
+    bash.arg(filename);
+    try {
+        bash.execSync();
+    } catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err);
+        throw "Failed to exec the .deisdownloader.sh which is deis downloader."
+    }
 }
 
-try {
-    deis.execSync();
-    tl.setResult(tl.TaskResult.Succeeded, "deis command success.");
-} catch (err) {
-    tl.setResult(tl.TaskResult.Failed, err)
-    throw "Failed to execute the deis command."
-}
-// deis command download and chmod
+login()  {
 
-// deis execute. 
+    let deis = new ToolRunner(tl.which(this.deisBinary, true));
+    deis.arg("auth:login");
+    deis.arg(this.serverURI);
+    deis.arg("--username=" + this.username);
+    deis.arg("--password=" + this.password);
+
+    try {
+        deis.execSync();
+    } catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err);
+        throw "Failed to login using deis auth:login command."
+    }
+}
+
+exec() {
+    this.execCommand();
+}
+async execCommand() : Promise<string> {
+
+    let deis = new ToolRunner(tl.which(this.deisBinary, true));
+    deis.arg(this.subCommand);
+    if (this.multilineArgs) {
+        this.multilineArgs.split(/\s+/).map(function (x) { deis.arg(x) });
+    }
+
+    try {
+        await deis.exec();
+        tl.setResult(tl.TaskResult.Succeeded, "deis command success.");
+    } catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err)
+        throw "Failed to execute the deis command."
+    }
+    return ""
+}
+
+}
+
+let command = new DeisCommand();
+command.init();
+command.exec();
+
+
 
